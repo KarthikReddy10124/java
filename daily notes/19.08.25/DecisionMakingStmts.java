fe153449c7eee32522ce6c8fcc99e2047a4ffcb8
KarthikReddy10124 <karthikreddy10124@gmail.com>
2025-08-19 22:20:49 +0530

    commit

---
+++ daily notes/19.08.25/DecisionMakingStmts.java
@@ -0,0 +1,12 @@
+public class DecisionMakingStmts {
+    public static void main(String[] args) {
+        int a= 2;
+        switch (a) {
+            case 1:System.out.println("a is matching to case 1");break;
+            case 2:System.out.println("a is matching to case 2");break;
+            case 3:System.out.println("a is matching to case 3");break;
+            
+            default:System.out.println("non of the cases are matched");
+        }
+    }
+}
---
+++ daily notes/19.08.25/Looping.java
@@ -0,0 +1,129 @@
+// // public class LoopingStmts {
+// //     public static void main(String[] args) {
+// //         // print hello for 100 times
+// //         //while
+
+// //         int i=1;
+// //         while (i<101) {
+// //             System.out.println("hello");
+// //             i++;
+// //         }
+// //     }
+// // }
+
+// public class LoopingStmts {
+//     public static void main(String[] args) {
+//         // print 1 to 100
+//         //while
+
+//         int i=1;
+//         while (i<101) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+// public class LoopingStmts {
+//     public static void main(String[] args) {
+//         // print hello for 100 times
+//         //while
+
+//         int i=1;
+//         while (i<101) {
+//             System.out.println("hello");
+//             i++;
+//         }
+//     }
+// }
+// public class Looping{
+//     public static void main(String[] args){
+//         for(int i=1;i<6;i++){
+//             for(int j=1;j<6;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class Looping{
+//     public static void main(String[] args){
+//         // do while loop
+// //         int i = 2;
+// //         do{
+// //             System.out.println("Welcome");
+// //             i++;
+// //         }while(i<10);
+        
+// //     }
+// // }
+
+// import java.util.Scanner;
+// public class Looping{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i=0;
+//         do{
+//             System.out.println("welcome user");
+//             System.out.println("greet agian ?");
+//             System.out.println("enter 1 to greet agian");
+//             i = sc.nextInt();
+//         }
+//         while(i==1);
+//     }
+// }
+
+
+
+// for loop
+// public class Looping{
+//     public static void main(String[] args){
+//         for (int i=0;i<5;i++){
+//             System.out.println(i+")Hello");
+//         }
+//     }
+// }
+
+// public class Looping{
+//     public static void main(String[] args){
+//         for (int i=1;i<11;i++){
+            
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// public class Looping{
+//     public static void main(String[] args){
+//         int sum=0;
+//         for (int i=1;i<=10;i++){
+//             sum=sum+i;
+//             System.out.println(sum);
+//         }
+//     }
+// // }
+
+// public class Looping{
+//     public static void main(String[] args){
+//         double sum=0;
+//         for (int i=1;i<=10;i++){
+//             sum=sum+i;
+//         }
+//         System.out.println(sum/10);
+//     }
+// }
+
+public class Looping{
+    public static void main(String[] args){
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=3;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
