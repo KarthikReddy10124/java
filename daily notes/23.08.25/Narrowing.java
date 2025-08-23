a9a59e2469f6ca340ceea6e0c357370b4787133d
KarthikReddy10124 <karthikreddy10124@gmail.com>
2025-08-23 12:25:19 +0530

    type casting

---
+++ daily notes/23.08.25/Narrowing.java
@@ -0,0 +1,11 @@
+
+public class Narrowing {
+    public static void main(String[] args) {
+        double b = 45.55;
+        int a = (int)b;//typecast operator+;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
---
+++ daily notes/23.08.25/Widening.java
@@ -0,0 +1,10 @@
+public class Widening {
+    public static void main(String[] args) {
+        int a = 10;
+        double b;
+        b = a; // implicit casting
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
