3637ea7813b6e43dd1fa5cb29587e0cc48c20d4f
KarthikReddy10124 <karthikreddy10124@gmail.com>
2025-08-30 15:06:02 +0530

    array programs

--- daily notes/30.08.25/FloydsPattern.java
+++ daily notes/30.08.25/FloydsPattern.java
@@ -1,6 +1,23 @@
+// public class FloydsPattern {
+//     public static void main(String[] args) {
+//         int n=5;
+//         int num=1;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(num+" ");
+//                 num++;
+//             }
+//             System.out.println();
+//         }
+//     }
+    
+// }
+
+import java.util.*;
 public class FloydsPattern {
     public static void main(String[] args) {
-        int n=5;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         int num=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
@@ -9,6 +26,6 @@ public class FloydsPattern {
             }
             System.out.println();
         }
+        sc.close();
     }
-    
 }
